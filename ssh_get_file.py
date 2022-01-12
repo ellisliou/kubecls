503996a64c904a3ced43008036731e37cfd7b5a5
ellis <ellisliou@iii.org.tw>
2022-01-12 17:17:40 +0800

    add sudo command

--- ssh_get_file.py
+++ ssh_get_file.py
@@ -10,6 +10,13 @@ user = 'k'
 pass1 = 'pp1234'
 
 ssh.connect(hostname=host, username=user, password=pass1, allow_agent = False)
+'''
+stdin, stdout, stderr =ssh.exec_command('sudo -i')
+stdin.write('pp1234')
+stdin.flush()
+stdin, stdout, stderr =ssh.exec_command('id')
+print(stdout.readlines())
+'''
 
 f = open('output.csv', 'w')
 writer = csv.writer(f)
@@ -17,7 +24,8 @@ writer = csv.writer(f)
 def write_to_csv(ssh,item_number,item_name,command):
 	
 	i, o, e = ssh.exec_command(command)
-	
+	i.write('pp1234'+ "\n")
+	i.flush()
 	line=o.readlines()
 	print(item_number,item_name,line[0])
 	list_tmp=[item_number,item_name,line[0][:-1]]
@@ -25,9 +33,8 @@ def write_to_csv(ssh,item_number,item_name,command):
 
 def main():
 	
-	write_to_csv(ssh,"1.1.1","Ensure that the API server pod  specification file permissions are set to 644 or more restrictive",'stat -c %a /etc/kubernetes/manifests/kube-apiserver.yaml')
-	write_to_csv(ssh,"1.1.2","Ensure that the API server pod specification file ownership 設置為 root:root",'stat -c %U:%G /etc/kubernetes/manifests/kube-apiserver.yaml')
-	
+	write_to_csv(ssh,"1.1.1","Ensure that the API server pod  specification file permissions are set to 644 or more restrictive",'sudo -S stat -c %a /etc/kubernetes/manifests/kube-apiserver.yaml')
+	write_to_csv(ssh,"1.1.2","Ensure that the API server pod specification file ownership 設置為 root:root",'sudo -S stat -c %U:%G /etc/kubernetes/manifests/kube-apiserver.yaml')	
 
 if __name__ == "__main__":
     main()
