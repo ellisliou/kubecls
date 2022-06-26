156c62d2b5425ead8c0af9cd12c43df3596fdf67
ellis <ellisliou@iii.org.tw>
2022-06-26 11:38:22 +0800

    add 5.1.3 ~ 5.1.8

--- connect.py
+++ connect.py
@@ -5,75 +5,187 @@ import yaml
 import re
 import glob
 
+def runAudit(num, command):
+    input, output, e = ssh.exec_command(command)
+    line=output.readlines()
+    if len(line) == 0:
+        line.insert(0,"")
+    return line
+
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 #pass1 = getpass.getpass('password: ')
 pass1='12'
 ssh.connect(hostname='192.168.118.209', username='k', password=pass1, allow_agent = False)
 
-file = open('./clusterrole.yaml')
-
-def runAudit(num, command):
-        input, output, e = ssh.exec_command(command)
-        line=output.readlines()
-        if len(line) == 0:
-            line.insert(0,"")
-        return line
-
 class k8s_config_check:
-    def __init__(self):
-        self.node = runAudit(0,"kubectl get no -o yaml")
-        file = open('k8s_node.yaml', 'w')
-        file.writelines(self.node)
-        file = open('./k8s_node.yaml')
-        self.node = yaml.load(file, Loader=yaml.FullLoader)
-        
-        self.clusterrolebindings = runAudit(0,"kubectl get clusterrolebindings -o yaml")
-        file = open('k8s_clusterrolebindings.yaml', 'w')
-        file.writelines(self.clusterrolebindings)
+    def __init__(self,yf):
+        self.yaml = yf["groups"]
 
-        self.rolebinding = runAudit(0,"kubectl get rolebinding -o yaml")
-        file = open('k8s_rolebinding.yaml', 'w')
-        file.writelines(self.rolebinding)
-        
-        self.ClusterRoles = runAudit(0,"kubectl get ClusterRoles -o yaml")
-        file = open('k8s_ClusterRoles.yaml', 'w')
-        file.writelines(self.ClusterRoles)
-        file = open('./k8s_ClusterRoles.yaml')
-        self.ClusterRoles = yaml.load(file, Loader=yaml.FullLoader)
-        
-        self.Roles = runAudit(0,"kubectl get Roles -o yaml")
-        file = open('k8s_Roles.yaml', 'w')
-        file.writelines(self.Roles)
-        file = open('./k8s_Roles.yaml')
-        self.Roles = yaml.load(file, Loader=yaml.FullLoader)
-
-    def secret_check(self,compareStr,comareItem):
-        #5.1.2 Minimize access to secrets
-        self.items=self.ClusterRoles["items"]
+    def secret_check(self,compareStr,comareItem,configYamlList):
+        secretList=""
+        if "/" in compareStr:
+            compareStr_list=compareStr.split("/")
+        else:
+            compareStr_list=[compareStr]
+        if configYamlList[3] is None: #k8s_ClusterRoles
+            self.items=""
+        else:
+            self.items=configYamlList[3]["items"]
+            self.binding=configYamlList[1]["items"]
         if self.items!="":
-            secretList="**ClusterRoles**\nName apiGroups verbs\n"
+            secretList="**ClusterRoles**\nUser_Name ClusterRole_Name apiGroups verbs\n"
             for i in range(len(self.items)):
-                for j in range(len(self.items[i]["rules"])):
-                    if comareItem in self.items[i]["rules"][j]:
-                        if compareStr in self.items[i]["rules"][j][comareItem]:
-                            secretList=secretList+self.items[i]["metadata"]["name"]
-                            if "apiGroups" in self.items[i]["rules"][j]:
-                                secretList=secretList+listToStr(self.items[i]["rules"][j]["apiGroups"])
-                            else:
-                                secretList=secretList+" "
-                            secretList=secretList+listToStr(self.items[i]["rules"][j]["verbs"])+"\n"
-        self.items=self.Roles["items"]
+                for k in range(len(self.items[i]["rules"])):
+                    if comareItem in self.items[i]["rules"][k]:
+                        for p in range(len(compareStr_list)):
+                            if compareStr_list[p] in self.items[i]["rules"][k][comareItem]:
+                                secretList=outputConcate(secretList,self.binding,self.items[i],k)
+        
+        if configYamlList[4] is None: #k8s_Roles
+            self.items=""
+        else:
+            self.items=configYamlList[4]["items"]
+            self.binding=configYamlList[2]["items"]
         if self.items:
-            secretList=secretList+"\n**Roles**\nName apiGroups verbs\n"
+            secretList=secretList+"\n**Roles**\nUser_Name Role_Name apiGroups verbs\n"
             for i in range(len(self.items)):
-                for j in range(len(self.items[i]["rules"])):
-                    if "resources" in self.items[i]["rules"][j]:
-                        if "secrets" in self.items[i]["rules"][j]["resources"]:
-                            secretList=secretList+self.items[i]["metadata"]["name"]
-                            secretList=secretList+listToStr(self.items[i]["rules"][j]["apiGroups"])
-                            secretList=secretList+listToStr(self.items[i]["rules"][j]["verbs"])+"\n"
+                for k in range(len(self.items[i]["rules"])):
+                    if comareItem in self.items[i]["rules"][k]:
+                        if compareStr in self.items[i]["rules"][k][comareItem]:
+                            secretList=outputConcate(secretList,self.binding,self.items[i],k)
         return secretList
+
+    def twoItem_check(self,compareStr,comareItem,compareStr2,comareItem2,configYamlList):
+        secretList=""
+        
+        if configYamlList[3] is None: #k8s_ClusterRoles
+            self.items=""
+        else:
+            self.items=configYamlList[3]["items"]
+            self.binding=configYamlList[1]["items"]
+        if self.items!="":
+            secretList="**ClusterRoles**\nUser_Name ClusterRole_Name apiGroups verbs\n"
+            for i in range(len(self.items)):
+                for k in range(len(self.items[i]["rules"])):
+                    self.rules=self.items[i]["rules"][k]
+                    if comareItem in self.rules and comareItem2 in self.rules:
+                        if compareStr in self.rules[comareItem] and compareStr2 in self.rules[comareItem2]:
+                            secretList=outputConcate(secretList,self.binding,self.items[i],k)
+        
+        if configYamlList[4] is None: #k8s_Roles
+            self.items=""
+        else:
+            self.items=configYamlList[4]["items"]
+            self.binding=configYamlList[2]["items"]
+        if self.items:
+            secretList=secretList+"\n================================================================\n**Roles**\nUser_Name Role_Name apiGroups verbs\n"
+            for i in range(len(self.items)):
+                for k in range(len(self.items[i]["rules"])):
+                    self.rules=self.items[i]["rules"][k]
+                    if comareItem in self.rules and comareItem2 in self.rules:
+                        if compareStr in self.rules[comareItem] and compareStr2 in self.rules[comareItem2]:
+                            secretList=outputConcate(secretList,self.binding,self.items[i],k)
+        return secretList
+
+    def serviceaccounts(self,audit_name,configYamlList):
+        secretList=""
+        if configYamlList[5] is None: #k8s_serviceaccounts
+            self.items=""
+        else:
+            self.items=configYamlList[5]["items"]
+        if self.items!="":
+            secretList="**Service_Accounts**\nName Namespace secrets\n"
+            for i in range(len(self.items)):
+                if self.items[i]["metadata"]["name"]==audit_name:
+                    secretList=secretList+self.items[i]["metadata"]["name"]+"  "
+                    secretList=secretList+self.items[i]["metadata"]["namespace"]+"  "
+                    for k in range(len(self.items[i]["secrets"])):
+                        secretList=secretList+self.items[i]["secrets"][k]["name"]
+                        if (k+1)!=len(self.items[i]["secrets"]):secretList=secretList+","
+                    secretList=secretList+"\n"
+        return secretList
+
+    def automountServiceAccountTokenCheck(self,configYamlList):
+        secretList=""
+        if configYamlList[6] is None: #k8s_pods
+            self.items=""
+        else:
+            self.items=configYamlList[6]["items"]
+        if self.items!="":
+            secretList="**Pods**\npod_Name Namespace\n"
+            for i in range(len(self.items)):
+                if "automountServiceAccountToken" not in self.items[i]["spec"]:
+                    secretList=secretList+self.items[i]["metadata"]["name"]+"  "
+                    secretList=secretList+self.items[i]["metadata"]["namespace"]+"  "
+                    secretList=secretList+"\n"
+                elif self.items[i]["spec"]["automountServiceAccountToken"]==true:
+                    secretList=secretList+self.items[i]["metadata"]["name"]+"  "
+                    secretList=secretList+self.items[i]["metadata"]["namespace"]+"  "
+                    secretList=secretList+"\n"
+        
+        if configYamlList[5] is None: #k8s_serviceaccounts
+            self.items=""
+        else:
+            self.items=configYamlList[5]["items"]
+        if self.items!="":
+            secretList=secretList+"================================================\n**Service_Accounts**\nName Namespace secrets\n"
+            for i in range(len(self.items)):
+                if "automountServiceAccountToken" not in self.items[i]:
+                    secretList=secretList+self.items[i]["metadata"]["name"]+"  "
+                    secretList=secretList+self.items[i]["metadata"]["namespace"]+"  "
+                    secretList=secretList+"\n"
+                elif self.items[i]["automountServiceAccountToken"]==true:
+                    secretList=secretList+self.items[i]["metadata"]["name"]+"  "
+                    secretList=secretList+self.items[i]["metadata"]["namespace"]+"  "
+                    secretList=secretList+"\n"
+        return secretList
+
+    def getMaxId(self):
+        return len(self.yaml)
+
+    def getMaxSubId(self,id):
+        return len(self.yaml[id]["checks"])
+
+class k8sChecks(k8s_config_check):
+    def __init__(self, id ,sub_id ,yf,configYamlList):
+        super().__init__(yf)
+        self.it = self.yaml[id]["checks"][sub_id]
+        self.id = self.it["id"]
+        self.text = self.it["text"]
+        if "audit" in self.it:
+            self.audit = self.it["audit"]
+            self.line = runAudit(self.id,self.audit)
+        elif "audit_function" in self.it:
+            if self.it["audit_function"] == "secret_check":
+                self.line =self.secret_check(self.it["compareStr"],self.it["comareItem"],configYamlList)
+            elif self.it["audit_function"] == "twoItem_check":
+                self.line =self.twoItem_check(self.it["compareStr"],self.it["comareItem"],self.it["compareStr2"],self.it["comareItem2"],configYamlList)
+            elif self.it["audit_function"] == "serviceaccounts":
+                self.line =self.serviceaccounts(self.it["audit_name"],configYamlList)
+            elif self.it["audit_function"] == "automountServiceAccountToken":
+                self.line =self.automountServiceAccountTokenCheck(configYamlList)
+        else:
+            self.line =""
+
+def outputConcate(secretList,binding,items,k):
+    bindingNameList=getBindingUserName(binding,items["metadata"]["name"])
+    for p in range(len(bindingNameList)):
+        secretList=secretList+bindingNameList[p]+"  "
+        secretList=secretList+items["metadata"]["name"]+"  "
+        if "apiGroups" in items["rules"][k]:
+            secretList=secretList+listToStr(items["rules"][k]["apiGroups"])+"  "
+        else:
+            secretList=secretList+"NONE  "
+        secretList=secretList+listToStr(items["rules"][k]["verbs"])+"\n"
+    return secretList
+
+def getBindingUserName(BindingYaml,roleName):
+    nameList=[]
+    for i in range(len(BindingYaml)):
+        if BindingYaml[i]["roleRef"]["name"]==roleName:
+            nameList.append(BindingYaml[i]["metadata"]["name"])
+    return nameList
 
 def listToStr(list):
     tmpstr="["
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from parseYaml import checks, parsed_yaml_file
+from connect import *
 import re
 import glob
 import yaml
@@ -7,6 +8,28 @@ import yaml
 f = open('output.csv', 'w')
 writer = csv.writer(f)
 f_ch5_test = open('ch5_test.txt', 'w')
+
+configYamlList= []
+k8sConfigList={
+"kubectl get no -o yaml":"k8s_node",
+"kubectl get clusterrolebindings -o yaml":"k8s_clusterrolebindings",
+"kubectl get rolebinding -o yaml":"k8s_rolebinding",
+"kubectl get ClusterRoles -o yaml":"k8s_ClusterRoles",
+"kubectl get Roles -o yaml":"k8s_Roles",
+"kubectl get serviceaccounts -A -o yaml":"k8s_serviceaccounts",
+"kubectl get pods -A -o yaml":"k8s_pods"
+}
+count=0
+for i in range(len(k8sConfigList)):
+    keyName=list(k8sConfigList.keys())[i]
+    configfile = runAudit(0,keyName)
+    file = open(k8sConfigList[keyName]+".yaml", 'w')
+    file.writelines(configfile)
+    file = open(k8sConfigList[keyName]+".yaml")
+    file = yaml.load(file, Loader=yaml.FullLoader)
+    configYamlList.append(file)
+    count+=1
+    #print(count)
 
 def loadAllTest():
     yamlList = []
@@ -33,14 +56,18 @@ def main():
 
     ch5_yaml = open('./ch5_policies_specific.yaml')
     ch5_yaml = yaml.load(ch5_yaml, Loader=yaml.FullLoader)
-    yf = parsed_yaml_file(ch5_yaml)
-    for k in range(yf.getMaxId()):
-        for i in range(yf.getMaxSubId(k)):
-            yf.check = checks(k,i,ch5_yaml)
-            f_ch5_test.writelines([yf.check.id+"\n",yf.check.text+"\n"])
+    #yf = parsed_yaml_file(ch5_yaml)
+    ch5Yf=k8s_config_check(ch5_yaml)
+    count=0
+    for k in range(ch5Yf.getMaxId()):
+        for i in range(ch5Yf.getMaxSubId(k)):
+            ch5Yf.check = k8sChecks(k,i,ch5_yaml,configYamlList)
+            f_ch5_test.writelines([ch5Yf.check.id+"\n",ch5Yf.check.text+"\n"])
             f_ch5_test.write("===Audit Result===\n")
-            f_ch5_test.writelines(yf.check.line)
+            f_ch5_test.writelines(ch5Yf.check.line)
             f_ch5_test.write("===Audit Result===\n\n")
+            count+=1
+            #print(count)
 
 
 if __name__ == "__main__":
